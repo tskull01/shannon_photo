c8cb1c04cc0e361f80012c253a730b0a16e7a462
tskull01 <tskulley29@yahoo.com>
2020-05-04 15:27:48 -0400

    serverless functions

--- functions/getfolders/getfolders.js
+++ functions/getfolders/getfolders.js
@@ -1,12 +1,11 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 
 var fs = require('fs');
-const path = '/assets/images';
+const path = './src/assets/images';
 let folders = [];
 function getFolders(){
   return new Promise((resolve, reject) => {
     fs.readdir(path, function (err, content) {
-      console.log(content[1]);
       folders = content; 
       if(folders.length = content.length){
         resolve(folders); 
